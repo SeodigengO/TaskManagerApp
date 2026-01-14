5ea187f8aba6d925c7cc3ca5519768ad3eb0c3d1
OSeodigeng <seodigengo@gmail.com>
2026-01-14 13:57:07 +0200

    Initial Commit: Add TaskManager projet structure and Task model

---
+++ src/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+
+    private int id;
+    private String title;
+    private boolean isCompleted;
+
+    Task(int id, String title){
+        this.id = id;
+        this.title = title;
+        this.isCompleted = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted() {
+        isCompleted = true;
+    }
+
+
+
+
+}
