d7091ff575231ceffdb7375476e05a3ef5cc288e
SeodigengO <seodigengo@gmail.com>
2026-05-18 12:31:06 +0200

    Refactor task manager app with Spring Boot concepts

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS "tasks";
+
+CREATE TABLE "tasks" (
+ "id" BIGINT NOT NULL,
+ "title" TEXT NOT NULL,
+ "status" TEXT NOT NULL DEFAULT "TODO",
+ "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+ CONSTRAINT "tasks_pk" PRIMARY KEY("id")
+
+
+CONSTRAINT tasks_status_chk
+    CHECK (status IN ("TODO", "IN_PROGRESS", "DONE"))
+
+);
